39379df04661bf077b09efa3fae01994f5abc225
hrs2203 <hrishabh2203@gmail.com>
2021-07-18 08:05:12 +0530

    react-bootstrap used and tested

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Spinner from 'react-bootstrap/Spinner';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
+import React, { useState, useEffect } from 'react';
+
 function App() {
+
+  const [per, setPer] = useState(-10);
+  const [strt, setStrt] = useState(false)
+
+  useEffect(() => {
+    if (!strt){
+      document.title = "Loading Data..."
+    }else {
+      document.title = `${per}% done`
+    }
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pd">
+      <Container>
+        <Card>
+          <Card.Body>
+            {
+              (strt === false) ? (
+                <Spinner animation="border" role="status" />
+              ) : (
+                <ProgressBar animated now={per} />
+              )
+            }
+            <hr />
+
+            <Row>
+              <Col>
+                <Button onClick={() => { 
+                  setStrt(true); setPer((per + 10));
+                }}> Proceed </Button>
+              </Col>
+              <Col>
+                <Button onClick={() => { 
+                  setStrt(false); setPer((-10));
+                }}> Reset </Button>
+              </Col>
+            </Row>
+
+
+
+
+          </Card.Body>
+        </Card>
+      </Container>
     </div>
   );
 }

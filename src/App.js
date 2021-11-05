5cd1f2aa75fb32dd3094262034f01e08bd261cac
hrishabhdubdub <hrishabh@dubdub.ai>
2021-11-05 22:18:42 +0530

    sampl routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import HomePage from "./components/HomePage"
+import HomePage from "./components/HomePage";
+import LoginPage from "./components/LoginPage";
+import RegisterPage from "./components/RegisterPage";
+
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
 	return (
-		<HomePage />
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={ <HomePage /> } />
+				<Route path="/login" element={ <LoginPage /> } />
+				<Route path="/register" element={ <RegisterPage /> } />
+				<Route path="*" element={ <div>404 Nothing found :| </div> } />
+			</Routes>
+		</BrowserRouter>
 	)
 }
